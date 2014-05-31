f6d25b389c7864024c840b13ab3d6c4270f478c4
Jared Zoneraich <jszoneraich@gmail.com>
2014-05-31 14:03:50 -0400

    front page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return render_template("index.html")
+	return render_template("index.html", page="index")
 @app.route('/signin', methods=['GET','POST'])
 def sign_in():
 	return render_template("signin.html")
